d2da4b5ad9106c6cc59a893b5230fe1827e96b15
prabhusingh851@gmail.com <eparis@atlassian.com>
2019-05-26 12:07:09 +0530

    minor changes.

--- app/src/main/java/com/myhexaville/login/login/SignUpFragment.java
+++ app/src/main/java/com/myhexaville/login/login/SignUpFragment.java
@@ -3,7 +3,6 @@ package com.myhexaville.login.login;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +19,9 @@ public class SignUpFragment extends Fragment implements OnSignUpListener{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View inflate = inflater.inflate(R.layout.fragment_signup, container, false);
 
 
-        return inflate;
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
 
     @Override
